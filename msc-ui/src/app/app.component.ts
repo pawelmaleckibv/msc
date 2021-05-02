603e10d4ce9551f3f0d3b4883b86fe3c8bd51fcb
pmalecki <net.pawel.malecki@mhp.com>
2021-05-02 13:42:22 +0200

    get data questionaryList

--- msc-ui/src/app/app.component.ts
+++ msc-ui/src/app/app.component.ts
@@ -10,19 +10,25 @@ import { QuestionaryDto } from './model-dto';
 })
 export class AppComponent implements OnInit{
   title = 'msc-ui';
-  questionary: Questionary[] = [];
+  questionaryList: Questionary[] = [];
   questionaryDto: QuestionaryDto[] = [];
 
   constructor(private quesionaryCtrl: QuesionaryCtrl) {
   }
 
   ngOnInit(): void {
-    this.quesionaryCtrl.getQuestionaries(20).subscribe( (questionary: Questionary[]) => {
-      this.questionary = questionary;
-    });
+    // this.quesionaryCtrl.getQuestionaries(20).subscribe( (questionary: Questionary[]) => {
+    //   this.questionary = questionary;
+    // });
 
     // this.quesionaryCtrl.getQuestionariesDto(20).subscribe( (questionaryDto: QuestionaryDto[]) => {
     //   this.questionaryDto = questionaryDto;
     // });
+  }
+
+  start(): void {
+    this.quesionaryCtrl.getQuestionaries(20).subscribe( (questionary: Questionary[]) => {
+      this.questionaryList = questionary;
+    });
   }
 }
--- msc-ui/src/app/app.module.ts
+++ msc-ui/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuesionaryCtrl } from './msc-controllers';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
